e3b8c1bb3be7d9c7a1eb1bd2c689d46b1de22491
Kaiwen Tian <kaiwen.tian@wishlife.com>
2021-07-24 22:30:08 +0800

    draw cube and line together

--- src/Three.js
+++ src/Three.js
@@ -8,30 +8,32 @@ export default function Three() {
 
     const lineMaterial = new THREE.LineBasicMaterial({ color: 0x0000ff });
     const points = [];
-    points.push(new THREE.Vector3(-10, 0, 0));
-    points.push(new THREE.Vector3(0, 10, 0));
-    points.push(new THREE.Vector3(10, 0, 0));
+    points.push(new THREE.Vector3(-2, 0, 0));
+    points.push(new THREE.Vector3(0, 2, 0));
+    points.push(new THREE.Vector3(2, 0, 0));
     const geometry = new THREE.BufferGeometry().setFromPoints(points);
-    const line = new THREE.Line( geometry, lineMaterial );
-    // const geometry = new THREE.BoxGeometry();
-    // const material = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
-    // const cube = new THREE.Mesh( geometry, material );
+    const line = new THREE.Line(geometry, lineMaterial);
+    
+    const boxGeometry = new THREE.BoxGeometry();
+    const meshMaterial = new THREE.MeshBasicMaterial({ color: 0x00ff00 });
+    const cube = new THREE.Mesh( boxGeometry, meshMaterial );
 
     const canvasWrapper = document.querySelector('.canvas-wrapper');
     console.log('canvasWrapper: ', canvasWrapper);
     let camera = null;
     if (!!canvasWrapper) {
-      camera = new THREE.PerspectiveCamera(75, canvasWrapper.offsetWidth / window.innerHeight, 0.1, 500);
+      camera = new THREE.PerspectiveCamera(95, canvasWrapper.offsetWidth / window.innerHeight, 0.1, 1000);
       renderer.setSize(canvasWrapper.offsetWidth, window.innerHeight);
-      camera.position.set(0, 0, 100);
+      camera.position.z = 5;
       canvasWrapper.appendChild(renderer.domElement);
       scene.add(line);
+      scene.add(cube);
     }
     
     function animate() {
-      // requestAnimationFrame(animate);
-      // cube.rotation.x += 0.01;
-      // cube.rotation.y += 0.01;
+      requestAnimationFrame(animate);
+      cube.rotation.x += 0.01;
+      cube.rotation.y += 0.01;
       renderer.render( scene, camera );
     }
     animate();
